3e7dcf120e1a517a6a5b8b37dc3ae788b653d491
Ryan <ryaneh173@yahoo.com>
2019-10-11 01:07:58 -0400

    progress

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,12 +1,28 @@
 #include "../includes_usr/fileIO.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries in books
  * 			SUCCESS if all data is loaded
  * */
-int loadBooks(std::vector<book> &books, const char* filename)
-{
+int loadBooks(std::vector<book> &books, const char *filename) {
+	books.clear();
+	ifstream file;
+	file.open(filename, ios::in);
+	if (!file.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	string line;
+	while (getline(file, line)) {
+		//TODO how to parse through line??
+	}
+
 	return SUCCESS;
 }
 
@@ -15,8 +31,17 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries books (do not create file)
  * 			SUCCESS if all data is saved
  * */
-int saveBooks(std::vector<book> &books, const char* filename)
-{
+int saveBooks(std::vector<book> &books, const char *filename) {
+	if (books.empty()) {
+		return NO_BOOKS_IN_LIBRARY;
+	}
+
+	ofstream file;
+	file.open(filename, ios::out);
+	if (!file.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
 	return SUCCESS;
 }
 
@@ -25,8 +50,7 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * 			NO_PATRONS_IN_LIBRARY if there are 0 entries in patrons
  * 			SUCCESS if all data is loaded
  * */
-int loadPatrons(std::vector<patron> &patrons, const char* filename)
-{
+int loadPatrons(std::vector<patron> &patrons, const char *filename) {
 	return SUCCESS;
 }
 
@@ -35,7 +59,17 @@ int loadPatrons(std::vector<patron> &patrons, const char* filename)
  * 			NO_PATRONS_IN_LIBRARY if there are 0 entries in patrons  (do not create file)
  * 			SUCCESS if all data is saved
  * */
-int savePatrons(std::vector<patron> &patrons, const char* filename)
-{
+int savePatrons(std::vector<patron> &patrons, const char *filename) {
+	if (patrons.empty()) {
+			return NO_PATRONS_IN_LIBRARY;
+		}
+
+		ofstream file;
+		file.open(filename, ios::out);
+		if (!file.is_open()) {
+			return COULD_NOT_OPEN_FILE;
+		}
+
+
 	return SUCCESS;
 }
